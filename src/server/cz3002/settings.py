35603d2f4809ecdb4f0cca6f8dc7e1bbb7d1b060
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2021-08-31 21:17:45 +0800

    update database info

--- src/server/cz3002/settings.py
+++ src/server/cz3002/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'cz3002.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cz3002',
+        'USER': 'user',
+        'PASSWORD': 'userpassword',
+
     }
 }
 
