7a72dc42c0e6ae7a958d694590a6f598dbc0e9c1
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2021-09-02 15:04:01 +0800

    remove debug path

--- src/server/backend/urls.py
+++ src/server/backend/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('user/login', csrf_exempt(views.auth_user), name='auth_user'),
     path('user/logout', csrf_exempt(views.logout_user), name='logout_user'),
     path('question/get', csrf_exempt(views.get_user_questions), name='get_questions'),
-    path('question/check', csrf_exempt(views.debug_check_auth), name='check_auth'),
     path('question/assign', csrf_exempt(views.assign_user_questions), name='assign_questions')
 ]
