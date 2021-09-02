c0d81c2a59aac3816825d9655f4e3568dd38a716
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2021-09-02 15:03:00 +0800

    added functions to assign and get question sets

--- src/server/backend/views.py
+++ src/server/backend/views.py
@@ -3,9 +3,9 @@ import uuid
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.hashers import make_password, check_password
-from .models import User, Userassignment
+from .models import User, Userassignment, Question
 from django.views.decorators.csrf import ensure_csrf_cookie
-import json , pickle
+import json
 
 
 # Create your views here.
@@ -21,11 +21,11 @@ def createuser(request):
                 return HttpResponse("invalid permissions", status=400)
         except KeyError:
             return HttpResponse("Please log in to create accounts", status=400)
-        current_role = int(request.session['role'])
         received_json_data = json.loads(request.body)
+        current_role = int(request.session['role'])
         username = received_json_data['username']
         password = received_json_data['password']
-        role = received_json_data['role']
+        role = int(received_json_data['role'])
         if role < current_role or role == 2:
             if not User.objects.filter(username=username):
                 if username is not None and password is not None and role is not None:
@@ -34,12 +34,12 @@ def createuser(request):
                     uuid_generated = False
                     while not uuid_generated:
                         user_uuid = uuid.uuid4()
-                        database_uuid = User.objects.filter(users=user_uuid)
+                        database_uuid = User.objects.filter(uuid=user_uuid)
                         if not database_uuid:
                             saved_user.uuid = user_uuid
                             uuid_generated = True
                     if 0 <= role <= 2:
-                        saved_user.role = int(role)
+                        saved_user.role = role
                     else:
                         return HttpResponse("invalid role!", status=400)
                     saved_user.username = username
@@ -54,6 +54,36 @@ def createuser(request):
             return HttpResponse("bad user creation request:invalid permissions", status=400)
 
 
+def debug_create_user(request):
+    received_json_data = json.loads(request.body)
+    username = received_json_data['username']
+    password = received_json_data['password']
+    role = int(received_json_data['role'])
+    if not User.objects.filter(username=username):
+        if username is not None and password is not None and role is not None:
+            saved_user = User()
+            password_hash = make_password(password)
+            uuid_generated = False
+            while not uuid_generated:
+                user_uuid = uuid.uuid4()
+                database_uuid = User.objects.filter(uuid=user_uuid)
+                if not database_uuid:
+                    saved_user.uuid = user_uuid
+                    uuid_generated = True
+            if 0 <= role <= 2:
+                saved_user.role = role
+            else:
+                return HttpResponse("invalid role!", status=400)
+            saved_user.username = username
+            saved_user.password = password_hash
+            saved_user.save()
+            return HttpResponse(status=201)
+        else:
+            return HttpResponse("bad user creation request", status=400)
+    else:
+        return HttpResponse("username already exists!", status=400)
+
+
 def auth_user(request):
     if request.method == 'POST':
         received_json_data = json.loads(request.body)
@@ -66,7 +96,8 @@ def auth_user(request):
                 request.session['authenticated'] = True
                 request.session['uuid'] = str(database_acc_search[0].uuid)
                 request.session['role'] = database_acc_search[0].role
-                return HttpResponse(status=200)
+                response = {}
+                return JsonResponse(response, status=200)
     return HttpResponse(status=400)
 
 
@@ -79,31 +110,62 @@ def logout_user(request):
         except KeyError:
             return HttpResponse(status=400)
 
-#TODO
+
+def debug_check_auth(request):
+    if request.method == 'POST':
+        try:
+            if request.session['authenticated']:
+                return HttpResponse(status=200)
+        except ValueError:
+            return HttpResponse(status=400)
+
+
 def assign_user_questions(request):
     if request.method == 'POST':
         try:
-            if request.session['authenticated'] and int(request.session['role'])=>1:
-                print("assign questions")
+            if request.session['authenticated'] and int(request.session['role']) >= 1:
+                received_json_data = json.loads(request.body)
+                if received_json_data:
+                    saved_user = User()
+                    userid = received_json_data['userid']
+                    database_uuid = User.objects.filter(uuid=userid)
+                    if not database_uuid:
+                        return HttpResponse("no user found", status=400)
+                    questions = received_json_data['questions']
+                    print(questions)
+                    # verify if input is reasonable
+                    verified = True
+                    for row in questions:
+                        if not row.isnumeric():
+                            verified = False
+                    if verified:
+                        saved_assignment = Userassignment()
+                        saved_assignment.userid = userid
+                        saved_assignment.questions = json.dumps(questions)
+                        saved_assignment.save()
+                        return HttpResponse(status=200)
+                    else:
+                        return HttpResponse("bad input", status=400)
+                else:
+                    return HttpResponse("bad input", status=400)
         except KeyError:
             return HttpResponse("Please Login", status=400)
 
-#TODO
+
 def get_user_questions(request):
     if request.method == 'GET':
         try:
             if request.session['authenticated']:
-                user_uuid = request.session['uuid']
-                database_assigned_questions = Userassignment()
-                saved_assignment = database_assigned_questions.objects.filter(userid=user_uuid)
+                user_uuid = request.GET.get('userid')
+                saved_assignment = Userassignment.objects.filter(userid=user_uuid)
                 if saved_assignment:
                     # return json with questions ids
 
                     # Get list of ids from database
-                    questions = pickle.loads(saved_assignment[0].questions)
+                    questions = json.loads(saved_assignment[0].questions)
                     json_data = {}
                     json_data['questions'] = questions
-                    return JsonResponse(json_data,status = 200)
+                    return JsonResponse(json_data, status=200)
 
                 else:
                     return HttpResponse("user has no assigned questions!", status=400)
