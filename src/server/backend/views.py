233c6da829478e4a6184305879de0aa99b0deb8e
robedpixel <18674579+robedpixel@users.noreply.github.com>
2021-09-02 09:44:38 +0800

    Update views.py to include user creation constraints

--- src/server/backend/views.py
+++ src/server/backend/views.py
@@ -16,38 +16,39 @@ def index(request):
 def createuser(request):
     if request.method == 'POST':
         try:
-            if request.session['role'] != 1:
+            if request.session['role'] == 0:
                 return HttpResponse("invalid permissions", status=400)
         except KeyError:
-            return HttpResponse("invalid permissions", status=400)
+            return HttpResponse("Please log in to create accounts", status=400)
+        currentrole = int(request.session['role'])
         username = request.POST.get('username')
         password = request.POST.get('password')
         role = request.POST.get('role')
-        if not User.objects.filter(username=username):
-            if username is not None and password is not None and role is not None:
-                saved_user = User()
-                password_hash = make_password(password)
-                uuid_generated = False
-                while not uuid_generated:
-                    user_uuid = uuid.uuid4()
-                    database_uuid = User.objects.filter(users=user_uuid)
-                    if not database_uuid:
-                        saved_user.users = user_uuid
-                        uuid_generated = True
+        if role < currentrole or role == 2:
+            if not User.objects.filter(username=username):
+                if username is not None and password is not None and role is not None:
+                    saved_user = User()
+                    password_hash = make_password(password)
+                    uuid_generated = False
+                    while not uuid_generated:
+                        user_uuid = uuid.uuid4()
+                        database_uuid = User.objects.filter(users=user_uuid)
+                        if not database_uuid:
+                            saved_user.users = user_uuid
+                            uuid_generated = True
 
-                if 0 <= role <= 1:
                     saved_user.role = int(role)
+                    saved_user.username = username
+                    saved_user.password = password_hash
+                    saved_user.save()
+                    return HttpResponse(status=201)
                 else:
                     return HttpResponse("bad user creation request", status=400)
-                saved_user.username = username
-                saved_user.password = password_hash
-                saved_user.save()
-                return HttpResponse(status=201)
             else:
-                return HttpResponse("bad user creation request", status=400)
+                return HttpResponse("username already exists!", status=400)
         else:
-            return HttpResponse("username already exists!", status=400)
-
+            return HttpResponse("bad user creation request:invalid permissions", status = 400)
+            
 
 def auth_user(request):
     if request.method == 'POST':
