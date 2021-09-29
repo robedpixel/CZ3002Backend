385da7f5d49e462779316a341f09f19042e58501
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2021-09-29 15:43:20 +0800

    update launcher script

--- src/server/runserver.bat
+++ src/server/runserver.bat
@@ -1,4 +1,4 @@
 %UserProfile%\Miniconda3\Scripts\activate.bat && ^
 conda activate CZ3002 && ^
 cd %~dp0 && ^
-python manage.py runserver_plus --cert cert.crt
+python manage.py runserver_plus --cert cert.crt 0.0.0.0:8000
